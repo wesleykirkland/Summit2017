ca0fa23b47abd477b47eaded41f957a630338217
Wesley Kirkland <wesley@wesleyk.me>
2017-04-23 12:56:25 -0400

    Same as last commit but on a different demo file

--- Meta Programming/Demo4_AD_Sites_And_Services_Backup.ps1
+++ Meta Programming/Demo4_AD_Sites_And_Services_Backup.ps1
@@ -3,26 +3,26 @@ $File4 = "C:\temp\meta\output\example4.ps1"
 
 #$ADReplicationSites = Get-ADReplicationSite -Filter * #Only run this if on the VPN
 
-$arrayfile = @()
-$arrayfile += 'Write-Warning this will restore a backup of AD sites and services subnets'
-$arrayfile += ''
+[System.Collections.ArrayList]$arrayfile = @()
+$arrayfile.Add('Write-Warning this will restore a backup of AD sites and services subnets') | Out-Null
+$arrayfile.Add('') | Out-Null
 
 foreach ($Site in $ADReplicationSites) {
     #Find all the subnets
     $temp = $Site.Name
     $ADReplicationSubnets = Get-ADReplicationSubnet -Filter {(Site -eq $temp)}
 
-    $arrayfile += "#Building command to build the Site $($Site.Name)" #Add a comment to the file
-    $arrayfile += "Set-ADReplicationSite -Identity $($Site.Name) -AutomaticInterSiteTopologyGenerationEnabled $true"
+    $arrayfile.Add("#Building command to build the Site $($Site.Name)") | Out-Null #Add a comment to the file
+    $arrayfile.Add("Set-ADReplicationSite -Identity $($Site.Name) -AutomaticInterSiteTopologyGenerationEnabled $true") | Out-Null
 
     foreach ($Subnet in $ADReplicationSubnets) {
-        #$arrayfile += "#Building command to build the subnet $($Subnet.Name)" #Add a comment to the file
-        $arrayfile += "Set-ADReplicationSubnet -Site $($Site.Name) -Identity '$($Subnet.Name)'$(if ($Site.Location) {" -Location '$($Site.Location)'"}) -ErrorAction SilentlyContinue" #Add location if it exists via a subexpression
+        #$arrayfile.Add("#Building command to build the subnet $($Subnet.Name)") | Out-Null #Add a comment to the file
+        $arrayfile.Add("Set-ADReplicationSubnet -Site $($Site.Name) -Identity '$($Subnet.Name)'$(if ($Site.Location) {" -Location '$($Site.Location)'"}) -ErrorAction SilentlyContinue") | Out-Null #Add location if it exists via a subexpression
     }
 
-    $arrayfile += ''
+    $arrayfile.Add('') | Out-Null
     Remove-Variable ADReplicationSubnets -ErrorAction SilentlyContinue
 }
 
-$arrayfile += "Write-Output 'End of the restore script'"
+$arrayfile.Add("Write-Output 'End of the restore script'") | Out-Null
 $arrayfile | Out-File .\output\example4.ps1
