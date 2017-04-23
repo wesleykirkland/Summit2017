f5a0a02ced482664003e607b4b81ba1870bc7f16
Wesley Kirkland <wesley@wesleyk.me>
2017-04-23 12:52:01 -0400

    Updated array adding from a crappy way to the proper way

--- Meta Programming/Demo2_Unit_Testing_Outline.ps1
+++ Meta Programming/Demo2_Unit_Testing_Outline.ps1
@@ -3,21 +3,22 @@ $File2 = "C:\temp\meta\output\example2.ps1"
 Remove-Item $File2 -ErrorAction SilentlyContinue
 $ModuleToBuildTests = 'AWSPowerShell'
 
-$arrayfile = @()
-$arrayfile += 'Describe "Unit testing outline" {'
+[System.Collections.ArrayList]$arrayfile = @()
+$arrayfile.Add('Describe "Unit testing outline" {') | Out-Null
 
 $ModuleCmdlets = Get-Command -Verb Get -Module $ModuleToBuildTests | Select-Object -ExpandProperty Name
 
 foreach ($Cmdlet in $ModuleCmdlets) {
-    $arrayfile += '     It "Testing {0} in the module of {1}"' -f $Cmdlet,$ModuleToBuildTests
-    $arrayfile += '          ({0}) | Should Be' -f $Cmdlet
-    $arrayfile += '     }'
+    $arrayfile.Add(('     It "Testing {0} in the module of {1}"' -f $Cmdlet,$ModuleToBuildTests)) | Out-Null
+    $arrayfile.Add(('          ({0}) | Should Be' -f $Cmdlet)) | Out-Null
+    $arrayfile.Add('     }') | Out-Null
+    
     if (!($Cmdlet -eq $ModuleCmdlets[-1])) {
         $arrayfile += ''
     }
 }
 
-$arrayfile += '}'
+$arrayfile.Add('}') | Out-Null
 
 $arrayfile | Add-Content -Path $File2
 
