765a4cdad5dd6022230df483b08e5dd5f495a2a3
Wesley Kirkland <wesley@wesleyk.me>
2018-04-11 20:32:24 -0400

    Updated Puppy Killer

--- Meta Programming/Demo1_Puppy_Killer.ps1
+++ Meta Programming/Demo1_Puppy_Killer.ps1
@@ -1,10 +1,11 @@
 ﻿$File = "C:\temp\meta\output\example1.ps1"
-$Colors = @('Black','DarkBlue','DarkGreen','DarkCyan','DarkRed','DarkMagenta','DarkYellow','Gray','DarkGray','Blue','Green','Cyan','Red','Magenta','Yellow','White')
-#[System.Collections.ArrayList]$Colors = (Get-Help -Name Write-Host -Parameter ForegroundColor | Out-String -Stream | Select-String -Pattern '- ' | Out-String).Split('-').Split().Where{$PSItem -notlike $null}
+#$Colors = @('Black','DarkBlue','DarkGreen','DarkCyan','DarkRed','DarkMagenta','DarkYellow','Gray','DarkGray','Blue','Green','Cyan','Red','Magenta','Yellow','White')
+[System.Collections.ArrayList]$Colors = (Get-Help -Name Write-Host -Parameter ForegroundColor | Out-String -Stream | Select-String -Pattern '- ' | Out-String).Split('-').Split().Where{$PSItem -notlike $null}
 
 Remove-Item $File -ErrorAction SilentlyContinue
 
 Write-Output "Write-Output 'Let the puppy killing commence'" | Add-Content -Path $File
+Write-Output "#Well it was fun while it lasted - https://twitter.com/jsnover/status/727902887183966208?lang=en"
 Write-Output 'pause' | Add-Content -Path $File
 
 foreach ($Num in 1..1000) {
